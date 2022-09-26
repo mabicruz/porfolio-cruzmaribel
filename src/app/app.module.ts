20c01bd874ba300f5bafd825ed8ab4365fe336c4
Enzo Marchesi <80432840+lemarchesi09@users.noreply.github.com>
2022-09-26 17:58:35 -0300

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
